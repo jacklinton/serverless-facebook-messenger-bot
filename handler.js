90c726dd6d8a136d88dd666a5891413ca9cb44d5
Jack Linton <jacklinton@mac.com>
2018-06-04 01:27:41 -0400

    Install dependencies and add joke logic.

--- handler.js
+++ handler.js
@@ -1,16 +1,41 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const axios = require('axios');
+const oneLinerJoke = require('one-liner-joke');
 
-  callback(null, response);
+module.exports.webhook = (event, context, callback) => {
+  if (event.method === 'GET' ) {
+    // facebook app verification
+    if (event.query['hub.verify_token'] ===
+    process.env.VERIFY_TOKEN && event.query['hub.challenge']) {
+      return callback(null, parseInt(event.query['hub.challenge'])
+      );
+    } else {
+      return callback(new Error('Invalid Token'));
+    }
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  if (event.method === 'POST') {
+    const entry = event.body.entry[0];
+    const messagingItem = entry.messaging[0];
+    if (!(messagingItem.message && messagingItem.message.text)) {
+      return callback(new Error('No message.'));
+    }
+
+    const joke = oneLinerJoke.getRandomJoke();
+    const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.ACCESS_TOKEN}`;
+
+    const payload = {
+      recipient: {
+        id: messagingItem.sender.id
+      },
+      message: {
+        text: joke.body
+      }
+    };
+
+    axios.post(url, payload)
+        .then(response => callback(null, response))
+        .catch(err => callback(err));
+  }
 };
